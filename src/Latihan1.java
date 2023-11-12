30fd93ad2f2b553b9799cd36c93df7f06138e957
AnnisaNurRamadhani <annisanurramadhani93@gmail.com>
2023-11-12 18:45:19 +0700

    commit Latihan1

---
+++ src/Latihan1.java
@@ -0,0 +1,23 @@
+public class Latihan1 {
+    public static void main(String [] args){
+        // 1. variabel harus didefinisikan terlebih dahulu
+        // 2. masukan nilai / value variabel tersebut
+        // 3. prses perhitungan / validasi
+        // 4. output
+
+        // 1. definisikan variabel
+        int angka1 ;
+        int angka2 ;
+        int hasil_perhitungan;
+
+        // 2. masukan nilai ke variabel
+        angka1 = 20;
+        angka2 = 4;
+
+        // 3. proses perhitungan
+        hasil_perhitungan = angka1 - angka2;
+
+        // 4. output
+        System.out.println("jadi hasilnya adalah" + hasil_perhitungan);
+    }
+}
